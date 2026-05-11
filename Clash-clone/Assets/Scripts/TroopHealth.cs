d10b232531ec89de54a8a281e240f5a7acd5508d
Ayush Jeevaraj <ayushj762@deltalearns.ca>
2026-05-11 14:40:31 -0700

    working on healthbar

--- Clash-clone/Assets/Scripts/TroopHealth.cs
+++ Clash-clone/Assets/Scripts/TroopHealth.cs
@@ -1,30 +1,45 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TroopHealth : MonoBehaviour
 {
     public int maxHealth = 200;
-    private int currentHealth;
+    public int currentHealth;
+
+    public GameObject healthBarPrefab;   // Drag your prefab here
+    private Image healthFill;            // The green bar
+    private Transform barTransform;      // The whole bar
 
     void Start()
-    {
-        currentHealth = maxHealth;
-    }
+{
+    currentHealth = maxHealth;
 
-    public void TakeDamage(int amount)
-    {
-        currentHealth -= amount;
-        Debug.Log("Troop took damage: " + amount + " | New HP = " + currentHealth);
+    // Instantiate as a child of the troop
+    GameObject bar = Instantiate(healthBarPrefab, transform);
+    bar.transform.localPosition = new Vector3(0, 2, 0); // above the head
+    bar.transform.localRotation = Quaternion.identity;
 
+    healthFill = bar.transform.Find("Background/Fill").GetComponent<Image>();
+}
+
+
+    public void TakeDamage(int dmg)
+    {
+        currentHealth -= dmg;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        // Update the green bar instantly
+        float percent = (float)currentHealth / maxHealth;
+        healthFill.fillAmount = percent;
+
+        // If dead → destroy troop
         if (currentHealth <= 0)
-            Die();
-    }
-
-    void Die()
-    {
-        Debug.Log("Troop died!");
-        Destroy(gameObject);
+        {
+            Destroy(gameObject);
+        }
     }
 }
+
 
 
 
